1dd3297df9d40623cf64e383ecbb4a023af4bcb9
Brian LaClair <brianlaclair@gmail.com>
2023-11-20 22:37:21 -0500

    Added social links, reworked configuration files

---
+++ branding/README.md
@@ -0,0 +1,9 @@
+All logos in this directory belong to their respective copyright holders, including:
+
+- Meta
+- Microsoft
+- ByteDance
+- Amazon
+- Google
+
+Please ensure that proper permissions and licenses are obtained before using these logos.
--- build.js
+++ build.js
@@ -1,7 +1,7 @@
 // build.js handles the process of importing data from /configuration into the page when loaded
 
 // Import Config File
-fetch('configuration/config.json')
+fetch('config.json')
     .then((response) => response.json())
     .then((json) => {
         // Set the page title
@@ -13,48 +13,71 @@ fetch('configuration/config.json')
 
         // Set inline content (profile picture, profile name, description)
         document.getElementById("profilePicture").src           = "media/" + json.image;
+        document.getElementById("background").src               = "media/" + json.image;
         document.getElementById("profileName").innerHTML        = json.title;
         document.getElementById("profileDescription").innerHTML = json.description;
 
-        links();
-    });
-
-function links() {
-    fetch('configuration/links.json')
-    .then((response) => response.json())
-    .then((json) => {
-        // Populate the links based on the links.json file
-        // We're building elements that look like this: <a class="linkOut" href="#"><div class="link"><img class="linkImage" src="media/avatar.png"><div class="linkText">Test Link 123</div></div></a>
-        // and placing them inside of linkList
-        var linkList = document.getElementById("linkList");
-
-        var jsonLinks = json.links;
-
-        for (var i = 0; i < jsonLinks.length; i++) {
-            var link = document.createElement("a");
-            link.setAttribute("class", "linkOut");
-            link.setAttribute("href", jsonLinks[i].url);
-
-            var linkElement = document.createElement("div");
-            linkElement.setAttribute("class", "link");
-
-            var linkImage = document.createElement("img");
-            linkImage.setAttribute("class", "linkImage");
-            linkImage.setAttribute("src", "media/" + jsonLinks[i].image);
-
-            var linkText = document.createElement("div");
-            linkText.setAttribute("class", "linkText");
-            linkText.innerHTML = jsonLinks[i].title;
-
-            if (jsonLinks[i].image !== "") {
-                linkElement.appendChild(linkImage);
-            }
-            linkElement.appendChild(linkText);
-            link.appendChild(linkElement);
-            linkList.appendChild(link);
+        // Convert all items in the style object to CSS variables
+        var style = json.style;
+        var styleKeys = Object.keys(style);
+        for (var i = 0; i < styleKeys.length; i++) {
+            document.documentElement.style.setProperty("--" + styleKeys[i], style[styleKeys[i]]);
         }
+
+        // For each item in the socials object, create a social link, where the image is the social icon from /branding
+        var socials = json.social;
+        var socialKeys = Object.keys(socials);
+        for (var i = 0; i < socialKeys.length; i++) {
+
+            if (socials[socialKeys[i]] !== "" && socials[socialKeys[i]] !== null) {
+                console.log(socials[socialKeys[i]]);
+                var social = document.createElement("a");
+                social.setAttribute("class", "socialLink");
+                social.setAttribute("href", socials[socialKeys[i]]);
+                social.setAttribute("target", "_blank");
+
+                var socialImage = document.createElement("img");
+                socialImage.setAttribute("class", "socialImage");
+                socialImage.setAttribute("src", "branding/" + socialKeys[i] + ".png");
+
+                social.appendChild(socialImage);
+                document.getElementById("socialLinks").appendChild(social);
+            }
+        }
+
+        links(json.links);
     });
-}
+
+function links(jsonLinks) {
+    // We're building elements that look like this: <a class="linkOut" href="#"><div class="link"><img class="linkImage" src="media/avatar.png"><div class="linkText">Test Link 123</div></div></a>
+    // and placing them inside of linkList
+    var linkList = document.getElementById("linkList");
+
+    for (var i = 0; i < jsonLinks.length; i++) {
+        var link = document.createElement("a");
+        link.setAttribute("class", "linkOut");
+        link.setAttribute("href", jsonLinks[i].url);
+
+        var linkElement = document.createElement("div");
+        linkElement.setAttribute("class", "link");
+
+        var linkImage = document.createElement("img");
+        linkImage.setAttribute("class", "linkImage");
+        linkImage.setAttribute("src", "media/" + jsonLinks[i].image);
+
+        var linkText = document.createElement("div");
+        linkText.setAttribute("class", "linkText");
+        linkText.innerHTML = jsonLinks[i].title;
+
+        if (jsonLinks[i].image !== "") {
+            linkElement.appendChild(linkImage);
+        }
+        linkElement.appendChild(linkText);
+        link.appendChild(linkElement);
+        linkList.appendChild(link);
+    }
+};
+
 
 function checkImage(image) {
     if (image.includes("http")) {
